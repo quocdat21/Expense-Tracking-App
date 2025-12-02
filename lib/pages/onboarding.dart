b296ce03e4dd36329b83672835e193ece856eaf8
QuocDat <datotis2103@gmail.com>
2025-12-02 15:57:22 +0700

    Improve onboarding UI

--- lib/pages/onboarding.dart
+++ lib/pages/onboarding.dart
@@ -12,28 +12,48 @@ class _OnboardingState extends State<Onboarding> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: const Color.fromRGBO(243, 236, 190, 1),
-      body: Container(
-        child: Column(
-          children: [
-            const SizedBox(
-              height: 120.0,
-            ),
-            Image.asset('assets/images/onboard.png'),
-            Container(
-              decoration: BoxDecoration(color: Colors.white),
+      body: Column(
+        children: [
+          const SizedBox(height: 120.0),
+          Image.asset('assets/images/onboard.png'),
+          Expanded(
+            child: Container(
+              margin: const EdgeInsets.only(left: 10, right: 10, bottom: 20),
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(60),
+              ),
               width: MediaQuery.of(context).size.width,
-              child: Column(
+              child: const Column(
                 children: [
-                  Text('Chi tiêu chủ động\nTài chính vững vàng',
+                  SizedBox(height: 20),
+                  Text(
+                    'Chi tiêu chủ động\nTài chính vững vàng',
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                        color: Colors.black,
+                        fontSize: 30,
+                        fontWeight: FontWeight.bold),
+                  ),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  Padding(
+                    padding: EdgeInsets.only(left: 15, right: 15),
+                    child: Text(
+                      'Expense Tracker là ứng dụng quản lý tài chính cá nhân đơn giản và hiệu quả, giúp bạn theo dõi thu nhập và các khoản chi tiêu hằng ngày một cách dễ dàng.',
+                      textAlign: TextAlign.center,
                       style: TextStyle(
                           color: Colors.black,
-                          fontSize: 30,
-                          fontWeight: FontWeight.bold))
+                          fontWeight: FontWeight.w500,
+                          fontSize: 18),
+                    ),
+                  )
                 ],
               ),
-            )
-          ],
-        ),
+            ),
+          )
+        ],
       ),
     );
   }
