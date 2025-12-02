27c679204ea2c3188b9f63a80ad39f43b4952f36
QuocDat <datotis2103@gmail.com>
2025-12-02 16:35:28 +0700

    Create new services support widget

---
+++ lib/services/support_widget.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class AppWidget {
+  static TextStyle headlineTextStyle(double size) {
+    return TextStyle(
+      color: Colors.black,
+      fontSize: size,
+      fontWeight: FontWeight.bold,
+    );
+  }
+}
