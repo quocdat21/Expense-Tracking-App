3d864e4f342c36b78968acf7603ea2897806a9fe
QuocDat <datotis2103@gmail.com>
2025-12-02 16:33:20 +0700

    Improve onboarding UI

--- lib/pages/onboarding.dart
+++ lib/pages/onboarding.dart
@@ -1,3 +1,4 @@
+import 'package:expense_tracking_app/services/support_widget.dart';
 import 'package:flutter/material.dart';
 
 class Onboarding extends StatefulWidget {
@@ -18,27 +19,24 @@ class _OnboardingState extends State<Onboarding> {
           Image.asset('assets/images/onboard.png'),
           Expanded(
             child: Container(
-              margin: const EdgeInsets.only(left: 10, right: 10, bottom: 20),
+              margin: const EdgeInsets.only(left: 13, right: 13, bottom: 26),
               decoration: BoxDecoration(
                 color: Colors.white,
                 borderRadius: BorderRadius.circular(60),
               ),
               width: MediaQuery.of(context).size.width,
-              child: const Column(
+              child: Column(
                 children: [
-                  SizedBox(height: 20),
+                  const SizedBox(height: 20),
                   Text(
                     'Chi tiêu chủ động\nTài chính vững vàng',
                     textAlign: TextAlign.center,
-                    style: TextStyle(
-                        color: Colors.black,
-                        fontSize: 30,
-                        fontWeight: FontWeight.bold),
+                    style: AppWidget.headlineTextStyle(30.0),
                   ),
-                  SizedBox(
+                  const SizedBox(
                     height: 20,
                   ),
-                  Padding(
+                  const Padding(
                     padding: EdgeInsets.only(left: 15, right: 15),
                     child: Text(
                       'Expense Tracker là ứng dụng quản lý tài chính cá nhân đơn giản và hiệu quả, giúp bạn theo dõi thu nhập và các khoản chi tiêu hằng ngày một cách dễ dàng.',
@@ -47,6 +45,24 @@ class _OnboardingState extends State<Onboarding> {
                           color: Colors.black,
                           fontWeight: FontWeight.w500,
                           fontSize: 18),
+                    ),
+                  ),
+                  const SizedBox(height: 40),
+                  Container(
+                    margin: const EdgeInsets.only(left: 50, right: 50),
+                    height: 68,
+                    decoration: BoxDecoration(
+                        color: const Color.fromRGBO(238, 95, 80, 1),
+                        borderRadius: BorderRadius.circular(60)),
+                    width: MediaQuery.of(context).size.width,
+                    child: const Center(
+                      child: Text(
+                        'Bắt Đầu',
+                        style: TextStyle(
+                            color: Colors.white,
+                            fontWeight: FontWeight.bold,
+                            fontSize: 28),
+                      ),
                     ),
                   )
                 ],
